75800570fb8ab126ca46fbb255fc7e3b4a0d6175
Abhishek Khangare <abhishekkhangare21@gmail.com>
2023-07-27 20:53:48 +0530

    changed landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,53 +6,32 @@ const LandingPage = () => {
   return (
     <div className="container m-auto lg:h-screen w-screen">
       <div className="flex h-full mx-8 lg:mx-0 pt-20  flex-col lg:flex-row">
-        <div className="flex-1">
-          <div className="lg:ml-36 text-center lg:text-start mb-12">
-            <h1 className=" ml-2 text-4xl text-white ">India / DGTAL</h1>
+        <div className="flex-1 mt-14">
+          <div className="lg:ml-36 text-center lg:text-start mb-8">
+            <h1 className=" ml-2 text-4xl text-white ">VIKASH</h1>
+            <h1 className=" ml-2 text-7xl text-white ">BOOKIE</h1>
           </div>
           <div className="flex flex-col lg:ml-36  text-white">
             <div className="flex justify-center lg:justify-start">
-              <button className="w-48   p-3 text-sm shadow-sm rounded-full  bg-[#1eb11e] hover:bg-[#fdcc52] ">
+              <button className="w-48 py-3 px-0 text-sm shadow-sm rounded-full  bg-[#1eb11e] hover:bg-[#d39804] ">
                 <BsWhatsapp className="inline text-xl mx-2" />
                 CONTACT US
               </button>
             </div>
-            <div className="flex justify-center lg:justify-start">
-              <button className="w-60  p-3 mt-6 text-sm  shadow-sm rounded-full  bg-[#1eb11e] hover:bg-[#fdcc52]">
+            {/* <div className="flex justify-center lg:justify-start">
+              <button className="w-60  py-3 px-0 mt-6 text-sm  shadow-sm rounded-full  bg-[#1eb11e] hover:bg-[#ce960a]">
                 <BsPaypal className="inline text-2xl mr-4 text-[#1f2650]" />
                 PAY NOW 999 INR
               </button>
-            </div>
+            </div> */}
           </div>
           <div className="mt-8 flex flex-col text-lg text-white">
             <p className="lg:ml-36 lg:mx-0 md:mx-20">
-              Grow your Business Use WhatsApp Bulk Message Software and connect
-              with your customers at any time.
-            </p>
-            <p className="lg:ml-36 lg:mx-0 md:mx-20">
-              Call Us - +91 80000 16218
-            </p>
-            <p className="lg:ml-36 lg:mx-0 md:mx-20">
-              1. This is a 1 Year Charge.
-            </p>
-            <p className="lg:ml-36 lg:mx-0 md:mx-20">2. No per Message cost.</p>
-            <p className="lg:ml-36 lg:mx-0 md:mx-20">
-              3. You can extract Numbers from your Joined Whatsapp Group.
-            </p>
-            <p className="lg:ml-36 lg:mx-0 md:mx-20">
-              4. You can Filter Whatsapp Numbers from Bulk Data.
-            </p>
-            <p className="lg:ml-36 lg:mx-0 md:mx-20">
-              5. You can send Bulk Messages with an individual's name.
-            </p>
-            <p className="lg:ml-36 lg:mx-0 md:mx-20">
-              6. You Can send JPG, Audio, Video, Text etc.
-            </p>
-            <p className="lg:ml-36 lg:mx-0 md:mx-20">
-              7. Send bulk Whatsapp without saving numbers in your Phone Book.
-            </p>
-            <p className="lg:ml-36 lg:mx-0 md:mx-20">
-              8. Join & Post in new Groups on a Single click.
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui
+              blanditiis praesentium voluptatum deleniti atque corrupti quos
+              dolores et quas molestias excepturi sint occaecati cupiditate non
+              provident, similique sunt in culpa qui officia deserunt mollitia
+              animi, id est laborum et dolorum fuga.
             </p>
           </div>
         </div>
